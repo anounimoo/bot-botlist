9d1a3857363ff389c180cd5aabd2c569e3317b3b
anounimoo <87666602+anounimoo@users.noreply.github.com>
2022-04-22 17:37:30 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,84 @@
+const Discord = require("discord.js"); 
+const client = new Discord.Client({intents: 32767});
+const config = require("./config.json"); 
+
+client.login(config.token);
+
+client.once('ready', async () => {
+
+    console.log(`✅ - Logado em ${client.user.username} com sucesso!\nLogado em: ${Date.now()}`)
+  
+    /*let a1 = [
+     "100% em slash commands!",
+    "Use /help para ver meus comandos!",
+    "Sou um bot de economia!"],
+    i = 0;
+    setInterval(() => client.user.setActivity(`${a1[i++ % a1.length]}`,{
+    type: "WATCHING"}
+    ), 15000)*/
+
+})
+
+module.exports = client;
+client.commands = new Discord.Collection();
+client.slashCommands = new Discord.Collection();
+client.config = require("./config.json");
+require("./handler")(client);
+const { glob } = require("glob");
+const { promisify } = require("util");
+
+const globPromise = promisify(glob);
+
+client.on("interactionCreate", async (interaction) => {
+
+    if (!interaction.guild) return;
+  
+    if (interaction.isCommand()) {
+
+        const cmd = client.slashCommands.get(interaction.commandName);
+
+        if (!cmd)
+            return;
+
+        const args = [];
+
+        for (let option of interaction.options.data) {
+
+            if (option.type === "SUB_COMMAND") {
+                if (option.name) args.push(option.name);
+                option.options?.forEach((x) => {
+                    if (x.value) args.push(x.value);
+                });
+            } else if (option.value) args.push(option.value);
+        }
+
+        cmd.run(client, interaction, args);
+    }
+
+    if (interaction.isContextMenu()) {
+        await interaction.deferReply({ ephemeral: false });
+        const command = client.slashCommands.get(interaction.commandName);
+        if (command) command.run(client, interaction);
+        
+    }
+});
+
+process.on('unhandledRejection', (reason, p) => {
+        console.log('Script Rejeitado:');
+        console.log(reason, p)
+    });
+    
+    process.on('uncaughtExceptionMonitor', (err, origin) => {
+        console.log('Bloquedo:');
+        console.log(err, origin)
+    });
+    
+    process.on('multipleResolves', (type, promise, reason) => {
+        console.log('Vários Erros:');
+        console.log(type, promise, reason)
+    });
+    
+    process.on("uncaughtException", (err, origin) => {
+        console.log('Catch Error:');
+        console.log(err, origin)
+    });
